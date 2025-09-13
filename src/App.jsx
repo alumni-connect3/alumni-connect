b8b69274b6f5799edc751e8fc2c4f8e9fd0e42c7
jagadeesh <jagadeeshkavuri3@gmail.com>
2025-09-13 11:29:25 +0530

    Initial commit: cleaned React project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "#f5f5f5",
+      fontFamily: "Arial, sans-serif",
+      flexDirection: "column"
+    }}>
+      <h1 style={{ color: "#333" }}>Welcome to My App</h1>
+      <p style={{ color: "#666" }}>This is your single clean home page.</p>
+    </div>
+  );
+}
+
+export default App;
